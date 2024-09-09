35c62af6e2b509f0a7d60d952361b6f949d99c6f
catherine4331 <catherine4331@gmail.com>
2024-09-09 22:46:05 +0100

    Different kind of random plot

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,31 @@
 use std::path::Path;
 
+use plotters::{
+    chart::{ChartBuilder, LabelAreaPosition},
+    prelude::{BitMapBackend, IntoDrawingArea},
+    series::LineSeries,
+    style::{BLUE, WHITE},
+};
 use rand::Rand;
 
-pub mod plot;
 pub mod rand;
 
 fn main() {
     let mut ran = Rand::new(10);
-    let mut r = 10.0;
 
-    let data: Vec<(f64, f64)> = (0..256)
-        .map(|_| {
-            let next = ran.gen();
+    let data: Vec<(i32, f64)> = (0..256).map(|i| (i, ran.gen())).collect();
 
-            let out = (r, next);
-            r = next;
+    let root_area = BitMapBackend::new(Path::new("./prng.png"), (600, 400)).into_drawing_area();
+    root_area.fill(&WHITE).unwrap();
 
-            out
-        })
-        .collect();
+    let mut ctx = ChartBuilder::on(&root_area)
+        .set_label_area_size(LabelAreaPosition::Left, 40)
+        .set_label_area_size(LabelAreaPosition::Bottom, 40)
+        .build_cartesian_2d(0..257, 0.0..1.1)
+        .unwrap();
 
-    plot::scatter(&data, Path::new("./prng.png"))
+    ctx.configure_mesh().draw().unwrap();
+
+    ctx.draw_series(LineSeries::new(data.iter().map(|point| *point), BLUE))
+        .unwrap();
 }
